eddedeb30d6856ef82589465ea5871dcdb6bbd55
Alexander Wright <Alexwright45177@gmail.com>
2025-03-17 23:31:45 -0400

    Update Address.java

--- src/model/Address.java
+++ src/model/Address.java
@@ -1,4 +1,12 @@
 package model;
 
 public class Address {
+    String street, city, state, zip;
+
+    public Address(String street, String city, String state, String zip) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
 }
