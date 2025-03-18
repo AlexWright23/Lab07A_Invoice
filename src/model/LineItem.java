11076e8b8ab6ace94df0f574fff47e631f874f1a
Alexander Wright <Alexwright45177@gmail.com>
2025-03-17 23:30:58 -0400

    Update LineItem.java

--- src/model/LineItem.java
+++ src/model/LineItem.java
@@ -1,4 +1,15 @@
 package model;
 
 public class LineItem {
+    Product product;
+    int quantity;
+
+    public LineItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public double getTotal() {
+        return quantity * product.unitPrice;
+    }
 }
