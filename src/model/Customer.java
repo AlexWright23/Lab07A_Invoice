ad3c8bb459b2e37b8865fe186e0cb221b93c1471
Alexander Wright <Alexwright45177@gmail.com>
2025-03-17 23:31:32 -0400

    Update Customer.java

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,4 +1,11 @@
 package model;
 
 public class Customer {
+    String name;
+    Address address;
+
+    public Customer(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
 }
