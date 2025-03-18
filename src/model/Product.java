27594adb8a0a2ed0f54a1a9b0c986190f3f97033
Alexander Wright <Alexwright45177@gmail.com>
2025-03-17 23:30:38 -0400

    Update Product.java

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,4 +1,11 @@
 package model;
 
 public class Product {
+    String name;
+    double unitPrice;
+
+    public Product(String name, double unitPrice) {
+        this.name = name;
+        this.unitPrice = unitPrice;
+    }
 }
